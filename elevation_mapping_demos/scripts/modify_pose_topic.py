Create custom launch life; Fix error in covariance node

elevation_mapping_demos/scripts/modify_pose_topic.py
@@ -2,21 +2,25 @@
 
 import rospy
 import geometry_msgs.msg
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
 import numpy as np
 
 
 def callback(msg):
-    PoseWithCovarianceStampedMessage = geometry_msgs.msg.PoseWithCovarianceStamped()
+    PoseWithCovarianceStampedMessage = PoseWithCovarianceStamped()
     PoseWithCovarianceStampedMessage.header = msg.header
     PoseWithCovarianceStampedMessage.pose.pose = msg.pose
     PoseWithCovarianceStampedMessage.pose.covariance = np.ones((4, 4))*0.005
 
 
 def main():
-    rospy.Subscriber("/poseStamped_node", geometry_msgs,
+    rospy.init_node('pose_modifier', anonymous=True)
+    rospy.Subscriber("/poseStamped_node", PoseStamped,
                      callback, queue_size=1)
-    rospy.Publisher("/poseStamped_with_covarwiance_node",
-                    geometry_msgs.msg.PoseWithCovarianceStamped, queue_size=1)
+    rospy.Publisher("/poseStamped_with_covariance_node",
+                    PoseWithCovarianceStamped, queue_size=1)
 
     rospy.spin()
 